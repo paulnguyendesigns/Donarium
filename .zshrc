10c85b84c592558668307221f8d6bbf6b3fb5193
paulnguyendesigns <paulxnguyen12@gmail.com>
2026-07-24 17:05:16 -0700

    working frontend,backend, and database connection

---
+++ .zshrc
@@ -0,0 +1,2 @@
+alias donarium='cd ~/Desktop/internship/github/Donarium/backend && source .venv/bin/activate'
+
--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import certifi
 
 load_dotenv()
 
 MONGODB_URI = os.getenv("MONGODB_URI")
 
-client = MongoClient(MONGODB_URI)
+client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
 
 database = client["donarium"]
